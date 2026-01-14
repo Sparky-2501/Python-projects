35212dcc6df400f73124690298d98bdaa0987ca0
prathamesh <prathamesh9b57@gmail.com>
2026-01-14 14:51:11 +0530

    initial commit

---
+++ 7.Blackjack/art.py
@@ -0,0 +1,31 @@
+logo = r"""
+ _     _            _    _            _    
+| |   | |          | |  (_)          | |   
+| |__ | | __ _  ___| | ___  __ _  ___| | __
+| '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+| |_) | | (_| | (__|   <| | (_| | (__|   < 
+|_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\
+                       _/ |                
+                      |__/                 
+------------Welcome to Blackjack!------------
+How to play:
+- Try to get as close to 21 without going over.
+- You and dealer have given 1-1 card each.
+- You can pass or get card.
+- You are only able to see computers one card
+- If the sum is less than 17 then dealer must get card.
+- You must reach at max than the computer to win.
+- You must do not exceed 21.
+
+Rules:
+-The deck is unlimited in size.
+-There are no jokers.
+-The Jack/Queen/King all count as 10.
+-The Ace can count as 11 or 1.
+-Use the following list as the deck of cards:
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+-The cards in the list have equal probability of being drawn.
+-Cards are not removed from the deck as they are drawn.
+-The computer is the dealer.
+
+"""
